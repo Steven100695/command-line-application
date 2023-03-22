3c77813145e104652fb5734ac2bbd4518e141063
Steven100695 <112973112+Steven100695@users.noreply.github.com>
2023-03-21 22:31:56 -0700

    api app edited

--- api.js
+++ api.js
@@ -1,35 +1,50 @@
 const superagent = require('superagent');
 const base = 'https://phone-specs-api.azharimm.dev';
 
+//search by brands
 const brand = async (brandName, page) => {
     // https://phone-specs-api.azharimm.dev/brands/apple-phones-48?page=1
     // Every brand and phones have their own id, we need those IDs for our API link.
     // Since the user will have no idea what those IDs are, we need to find it ourself.
     try {
-        // First, we list all the brand
+        // List all the brand
         const brandURL = `${base}/brands`;
         const res = await superagent.get(brandURL);
-        // Next, we find the requsted brands object by finding the brand name
+        // Find the requsted brand object
         const brand = res.body.data.find((brand) => brand.brand_name.toLowerCase().includes(brandName.toLowerCase()));
-        // Get the object brand_id
-        const brandId = brand.brand_id;
-
-        // Voila! We got our link.
-        const brandNameURL = `${base}/brands/${brandName}-phones-${brandId}?page=${page}`;
+        // Get the object brand_slug
+        // brand_slug contain `brand_name-phones-brand_id`
+        const brandNameURL = `${base}/brands/${brandName}-phones-${brand.brand_slug}?page=${page}`;
         const resquest = await superagent.get(brandNameURL);
-        console.log(resquest.body);
+        //console.log(resquest.body);
+        return resquest.body;
     } catch (error) {
         console.log(error);
     }
 };
 
+//search by model
 const model = async (modelsName) => {
     // https://phone-specs-api.azharimm.dev/search?query=iPhone_12_pro_max
     try {
         const modelURL = `${base}/search?query=${modelsName}`;
-        console.log(modelURL);
+        //console.log(modelURL);
 
         const res = await superagent.get(modelURL);
+        //console.log(res.body);
+
+        return res.body;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const itemDetail = async (slug) => {
+    try {
+        const specsURL = `${base}/${slug}`;
+        console.log(specsURL);
+
+        const res = await superagent.get(specsURL);
         console.log(res.body);
 
         return res.body;
@@ -38,4 +53,8 @@ const model = async (modelsName) => {
     }
 };
 
-model('apple ipad pro 12');
+module.exports = {
+    brand,
+    model,
+    itemDetail
+}
---
+++ app.js
@@ -0,0 +1,9 @@
+const api = require('./api.js');
+
+const searchPhone = async (args) => {
+    const a = 0;
+}
+
+module.exports = {
+    searchPhone
+};
